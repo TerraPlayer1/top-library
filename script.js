05c8c9edb728d8b6c27a55dd9a0c337c3102ead9
TerraPlayer1 <terragamer70@gmail.com>
2023-03-01 09:29:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.info = function () {
+    return `${title} by ${author}, ${pages}, ${status}`;
+  };
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
